5566d6f34092b7b4ec0acff2c2f9367821ad7f57
Yoshitaka-Nagai <129018871+Yoshitaka-Nagai@users.noreply.github.com>
2023-05-17 13:36:42 +0900

    works個別ページのwebsiteへのリンクボタン修正

--- js/worksIntroduction.js
+++ js/worksIntroduction.js
@@ -1,12 +1,3 @@
 document.addEventListener('DOMContentLoaded', function () {
-  //ヒントアイコン表示エリア
-  const hintArea = document.getElementById('site-view__hint-area');
   
-  //画像スクロールエリア
-  const scrollArea = document.getElementById('site-view__scroll-area');
-
-  //画像部分がスクロールされたらヒントエリア非表示
-  scrollArea.addEventListener('scroll', function(){
-    hintArea.classList.add('js-hidden');
-  });
 }, false);
