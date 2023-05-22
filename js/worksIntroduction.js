78adfa3dc72c8665665ee39b0afc3924bda78269
Yoshitaka-Nagai <129018871+Yoshitaka-Nagai@users.noreply.github.com>
2023-05-22 23:51:51 +0900

    スクロールバー作成（リサイズ対応まだ）

--- js/worksIntroduction.js
+++ js/worksIntroduction.js
@@ -1,98 +1,148 @@
 //WORKS個別ページ用JS
 document.addEventListener('DOMContentLoaded', function () {
   //サイトスクリーンショットのスクロールエリア
-  const scrollArea = document.getElementById('site-view__scroll-area');
+  const scrollInner = document.getElementById('site-view__scroll-area');
+
+  //スクロールバーのハンドル
+  const Thumb = document.getElementById('site-view__scrollbar-thumb');
 
   //スクロールバー
   const scrollbar = document.getElementById('site-view__scrollbar');
 
-  //スクロールバーのハンドル
-  const scrollbarThumb = document.getElementById('site-view__scrollbar-thumb');
-
   //スクロールエリアの高さ
-  let scrollAreaHeight = scrollArea.clientHeight;
+  let scrollableHeight = scrollInner.clientHeight;
 
   //スクロールエリア内部も含めた高さ
-  let totalHeight = scrollArea.scrollHeight;
-
-  //スクロール可能量
-  let scrollableValue = totalHeight - scrollAreaHeight;
-
-  //ハンドルの現在のTOPからの位置：%
-  let currentThumbPositionPercent = 0;
-
-  //ハンドルの現在のTOPからの位置：px
-  let currentThumbPositionPx = 0;
-
-  //ドラッグ制御フラグ
-  let isActive = false;
+  let totalHeight = scrollInner.scrollHeight;
 
   //
-  let beforeMoveY;
+  let barHeight = 10;
 
-  scrollbarThumb.addEventListener('mousedown', function (e) {
-    isActive = true;
-    beforeMoveY = e.pageY;
-  });
+  //
+  let scrollbarTrack = scrollableHeight - barHeight;
 
-  scrollbarThumb.addEventListener('mousemove', function (e) {
-    if (!isActive) {
+  //ドラッグ制御フラグ
+  let active = false;
+
+  let scrollbar_thumb_cursor_Y;
+
+
+
+
+
+
+
+  scrollInner.addEventListener('scroll', function () {
+    if (active) {
       return;
     }
+    const y = (scrollInner.scrollTop * scrollbarTrack) / (totalHeight - scrollableHeight);
+    Thumb.style.transform = "translate(-50%, " + y + "px)";
+  }, { passive: true }
+  );
 
-    const delta = e.pageY - beforeMoveY;
-    let resultPositionPx = currentThumbPositionPx + delta;
-    if (resultPositionPx < 0) {
-      resultPositionPx = 0;
-    } else if (resultPositionPx > scrollAreaHeight) {
-      resultPositionPx = scrollAreaHeight;
+  scrollbar.addEventListener('click', function (event) {
+    console.log("click");
+    event.preventDefault();
+    active = true;
+    var calc = event.layerY - barHeight / 2;
+    scrollbar_thumb_Y = event.layerY;
+    var elemElem = scrollbar_thumb_Y - barHeight / 2;
+    var calc =
+      (totalHeight - scrollableHeight) / (scrollableHeight - barHeight);
+    var yy = elemElem * calc;
+    if (elemElem < 0) { elemElem = 0; } else if (elemElem > scrollbarTrack) {
+      elemElem = scrollbarTrack;
     }
+    Thumb.style.transform = "translate(-50%, " + elemElem + "px)";
+    scrollInner.scrollTop = yy;
+    active = false;
+  },
+    { passive: false }
+  );
 
-    scrollbarThumb.style.top = resultPositionPx + 'px';
-    currentThumbPositionPx = resultPositionPx;
+  Thumb.addEventListener('mousedown', function (event) {
+    console.log("mousedown");
+    active = true;
+    scrollbar_thumb_cursor_Y = event.pageY - this.getBoundingClientRect().top;
+  },
+    { passive: true }
+  );
+
+  document.addEventListener('mousemove', function (event) {
+    event.preventDefault();
+    if (!active) return;
+    event = event.pageY;
+    var scrollbar_thumb_Y =
+      ((event - scrollbar.getBoundingClientRect().top) / scrollbarTrack) *
+      scrollbarTrack -
+      scrollbar_thumb_cursor_Y;
+    var calc =
+      (totalHeight - scrollableHeight) / (scrollableHeight - barHeight);
+    var yy = scrollbar_thumb_Y * calc;
+    if (scrollbar_thumb_Y < 0) { scrollbar_thumb_Y = 0; } else if (scrollbar_thumb_Y > scrollbarTrack) {
+      scrollbar_thumb_Y = scrollbarTrack;
+    }
+    Thumb.style.transform = "translate(-50%, " + scrollbar_thumb_Y + "px)";
+    scrollInner.scrollTop = yy;
+  },
+    { passive: false }
+  );
+
+  document.addEventListener("mouseup", function (event) {
+    console.log("mouseup");
+    active = false;
   });
 
-  scrollbarThumb.addEventListener('mouseup', function () {
-    isActive = false;
-    console.log('mouseup');
-  });
-
-  scrollbarThumb.addEventListener('mouseleave', function () {
-    isActive = false;
-    console.log('leave');
-  });
 
 
 
-  //ウィンドウのリサイズ時処理
-  window.addEventListener('resize', function () {
-    //スクリーンショット画像の高さを再取得
-    totalHeight = scrollArea.scrollHeight;
-    scrollAreaHeight = scrollArea.clientHeight;
-    scrollableValue = totalHeight - scrollAreaHeight;
 
-    //ハンドルの位置設定
-    setScrollbarThumbPositon();
-  });
 
-  //エリアのスクロールイベント時処理
-  scrollArea.addEventListener('scroll', function () {
-    //ハンドルの位置設定
-    setScrollbarThumbPositon();
-  });
 
-  //スクロールバーハンドルの位置設定
-  function setScrollbarThumbPositon() {
-    //エリア内のスクロール量
-    const scrollValue = scrollArea.scrollTop;
 
-    //位置の%
-    currentThumbPositionPercent = Math.floor(scrollValue / scrollableValue * 100);
 
-    //位置のpx
-    currentThumbPositionPx = Math.floor(scrollAreaHeight * (currentThumbPositionPercent / 100));
 
-    //スタイル設定
-    scrollbarThumb.style.top = currentThumbPositionPx + 'px';
-  }
+  // //スクロール可能量
+  // let scrollableValue = totalHeight - scrollableHeight;
+
+  // //ハンドルの現在のTOPからの位置：%
+  // let currentThumbPositionPercent = 0;
+
+  // //ハンドルの現在のTOPからの位置：px
+  // let currentThumbPositionPx = 0;
+
+
+
+  // //ウィンドウのリサイズ時処理
+  // window.addEventListener('resize', function () {
+  //   //スクリーンショット画像の高さを再取得
+  //   totalHeight = scrollInner.scrollHeight;
+  //   scrollableHeight = scrollInner.clientHeight;
+  //   scrollableValue = totalHeight - scrollableHeight;
+
+  //   //ハンドルの位置設定
+  //   setScrollbarThumbPositon();
+  // });
+
+  // //エリアのスクロールイベント時処理
+  // scrollInner.addEventListener('scroll', function () {
+  //   //ハンドルの位置設定
+  //   setScrollbarThumbPositon();
+  // });
+
+  // //スクロールバーハンドルの位置設定
+  // function setScrollbarThumbPositon() {
+  //   //エリア内のスクロール量
+  //   const scrollValue = scrollInner.scrollTop;
+
+  //   //位置の%
+  //   currentThumbPositionPercent = Math.floor(scrollValue / scrollableValue * 100);
+
+  //   //位置のpx
+  //   currentThumbPositionPx = Math.floor(scrollableHeight * (currentThumbPositionPercent / 100));
+
+  //   //スタイル設定
+  //   Thumb.style.top = currentThumbPositionPx + 'px';
+  // }
 }, false);
